b68b6684a2ee98fb0f59e0e0113d0125e90524f7
smslsmls <skipp1223@naver.com>
2023-08-27 19:06:19 +0900

    Step 4-8 패턴 생성 및 무게 중심 구하기

--- gSolution/gPrj/CDlgImage.cpp
+++ gSolution/gPrj/CDlgImage.cpp
@@ -73,10 +73,10 @@ void CDlgImage::OnPaint()
 
 void CDlgImage::InitImage()
 {
-	//int nWidth = 640;
-	//int nHeight = 480;
-	int nWidth = 4096*5;
-	int nHeight = 4096*5;
+	int nWidth = 640;
+	int nHeight = 480;
+	//int nWidth = 4096*5;
+	//int nHeight = 4096*5;
 	int nBpp = 8;
 
 	m_image.Create(nWidth, -nHeight, nBpp);
--- gSolution/gPrj/gPrjDlg.cpp
+++ gSolution/gPrj/gPrjDlg.cpp
@@ -72,6 +72,8 @@ BEGIN_MESSAGE_MAP(CgPrjDlg, CDialogEx)
 	ON_WM_DESTROY()
 	ON_BN_CLICKED(IDC_BTN_TEST, &CgPrjDlg::OnBnClickedBtnTest)
 	ON_BN_CLICKED(IDC_BTN_PROCESS, &CgPrjDlg::OnBnClickedBtnProcess)
+	ON_BN_CLICKED(IDC_BTN_MAKE_PATTERN, &CgPrjDlg::OnBnClickedBtnMakePattern)
+	ON_BN_CLICKED(IDC_BTN_GET_DATA, &CgPrjDlg::OnBnClickedBtnGetData)
 END_MESSAGE_MAP()
 
 
@@ -244,4 +246,57 @@ void CgPrjDlg::OnBnClickedBtnProcess()
 	auto millisec = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
 	cout << nRet << "\t" << millisec.count() << endl;
+}
+
+
+void CgPrjDlg::OnBnClickedBtnMakePattern()
+{
+	unsigned char* fm = (unsigned char*)m_pDlgImage->m_image.GetBits();
+	int nWidth = m_pDlgImage->m_image.GetWidth();
+	int nHeight = m_pDlgImage->m_image.GetHeight();
+	int nPitch = m_pDlgImage->m_image.GetPitch();
+
+	memset(fm, 0, nWidth * nHeight);
+
+	CRect rect(100, 100, 200, 200);
+	for (int j = rect.top; j < rect.bottom; j++)
+	{
+		for (int i = rect.left; i < rect.right; i++)
+		{
+			fm[j * nPitch + i] = rand()%0xff;
+		}
+	}
+
+	m_pDlgImage->Invalidate();
+}
+
+void CgPrjDlg::OnBnClickedBtnGetData()
+{
+	unsigned char* fm = (unsigned char*)m_pDlgImage->m_image.GetBits();
+	int nWidth = m_pDlgImage->m_image.GetWidth();
+	int nHeight = m_pDlgImage->m_image.GetHeight();
+	int nPitch = m_pDlgImage->m_image.GetPitch();
+
+	int nTh = 0x80;
+	CRect rect(0, 0, nWidth, nHeight);
+	int nSumX = 0;
+	int nSumY = 0;
+	int nCount = 0;
+	for (int j = rect.top; j < rect.bottom; j++)
+	{
+		for (int i = rect.left; i < rect.right; i++)
+		{
+			if (fm[j * nPitch + i] > nTh)
+			{
+				nSumX += i;
+				nSumY += j;
+				nCount++;
+			}
+		}
+	}
+
+	double dCentorX = (double)nSumX / nCount;
+	double dCentorY = (double)nSumY / nCount;
+
+	cout << dCentorX << "\t" << dCentorY << endl;
 }
